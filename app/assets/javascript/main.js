3926c5af4532f226a729c5f12239ba4106bf661a
Paul Flynn <pgflynn@gmail.com>
2019-12-02 00:22:30 +0000

    Tweaks to search

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -2,69 +2,99 @@
 // ES6 or Vanilla JavaScript
 
 const healthAz = [];
-let results = document.querySelector('.new-search-results');
+const searchWrapper = document.querySelector('.nhsuk-header__search');
 const reusltsWrapper = document.querySelector('.new-search-results-wrapper');
+let results = document.querySelector('.new-search-results');
 reusltsWrapper.style.display = "none";
-// let filteredTotal = document.querySelector('.filtered-total');
-
-
 
 fetch("/javascript/conditions.json")
     .then(response => response.json())
     .then(data => healthAz.push(...data));
     // console.log(healthAz);
+
     
 function findMatches(searchTerm, healthAz) {
     return healthAz.filter(condition => {
         const regex = new RegExp(searchTerm, 'gi');
-        // console.log(regex);
-        return condition.title.match(regex) || condition.synonyms.match(regex) || condition.misspelling.match(regex) || condition.tag.match(regex);
-        // return condition.synonyms.match(regex);
-    })    
+        return condition.title.match(regex) || condition.synonyms.match(regex) || condition.misspelling.match(regex) || condition.tag.match(regex);        
+    })  
+    
+    // const filteredResult = healthAz.filter((item) => {
+    //     return (item.synonyms.indexOf(searchTerm) >= 0);
+    // });
+
+    console.log(filteredResult);
+
 }
 
 function showMatches() {
     const matchArray = findMatches(this.value, healthAz);
-    // console.log(matchArray);
     const checkLength = searchConditions.value;
-    matchArray.length;
+    // matchArray.length;
 
     const html = matchArray.map(condition => {
         const regex = new RegExp(this.value, 'gi');
         const conditionTitle = condition.title.replace(regex, `<span class="search_highlight">${this.value}</span>`);
-        const conditionSynonyms = condition.synonyms.replace(regex, `<span class="search_highlight">${this.value}</span>`);
-        const conditionMisspelling = condition.misspelling.replace(regex, `<span class="search_highlight">${this.value}</span>`);
-        
+        // const conditionSynonyms = condition.synonyms.replace(regex, `<span class="search_highlight">${this.value}</span>`);
+        // const conditionMisspelling = condition.misspelling.replace(regex, `<span class="search_highlight">${this.value}</span>`);
+
         return `
-            <li class='nhsuk-list-panel__item'>
-                <a class="nhsuk-list-panel__link">
+        ${(() => {
+          if (regex.test(condition.title)) {
+            return `
+              <li class='nhsuk-list-panel__item'>
+                  <a class="nhsuk-list-panel__link">
                     ${conditionTitle} 
-                    <em>${conditionSynonyms ? `${conditionSynonyms}` : ``} </em>
-                    <em>${conditionMisspelling ? `${conditionMisspelling}` : ``}</em>
-                </a>
-            </li>            
-        `;
-    }).join('');
+                  </a>
+              </li>
+            `
+          } else if (regex.test(condition.synonyms)) {
+
+            // console.log(condition.synonyms);
+            // let toArray = condition.synonyms.split(", ");
+            // const match = toArray.find(value => regex.test(value));
+            // console.log(match);
+
+            return `
+              <li class='nhsuk-list-panel__item'>
+                  <a class="nhsuk-list-panel__link">
+                    ${conditionTitle} 
+                    - <span class="search_highlight">${this.value}</span>
+                  </a>
+              </li>
+            `
+          } 
+        })()}        
+    `;
+    
+  })
+  .join("");
+
+
+    // const html = matchArray.map(condition => {
+    //     const regex = new RegExp(this.value, 'gi');
+    //     const conditionTitle = condition.title.replace(regex, `<span class="search_highlight">${this.value}</span>`);
+    //     const conditionSynonyms = condition.synonyms.replace(regex, `<span class="search_highlight">${this.value}</span>`);
+    //     const conditionMisspelling = condition.misspelling.replace(regex, `<span class="search_highlight">${this.value}</span>`);
+    //     return [condition.title, condition.synonyms, condition.misspelling];
+    // });
+
+    // const htmlShort = html.slice(0, 5);
+    // console.log(htmlShort);
 
     if (checkLength.length >= 1) {
         results.innerHTML = html;
         reusltsWrapper.style.display = "block";
-        // filteredTotal.style.display = "inlineBlock";
-        // searchClear.style.display = "block";
-        // filteredTotal.innerHTML = `
-        //     <strong><span style="color: #000;">${matchArray.length}</span></strong> matches
-        // `;     
         results.style.display = "block";
     } else {
         reusltsWrapper.style.display = "none";
-        // filteredTotal.innerHTML = " ";
     }
     
     if ((checkLength.length >= 1) && (matchArray.length === 0)) {
         results.innerHTML = `
         <li class='nhsuk-list-panel__item'>
             <a class="nhsuk-list-panel__link">
-                No matches, please try filtering or check the spelling.
+                No matches, please or check the spelling or check the A-Z.
             </a>
         </li>
     `;
@@ -76,103 +106,13 @@ const searchConditions = document.querySelector('.new-search__input');
 searchConditions.addEventListener('change', showMatches);
 searchConditions.addEventListener('keyup', showMatches);
 
+/*
+//as arrays 
+"synonyms": ["Runs", "shits", "squits"],
+"misspelling": ["Diarhoea", "Diarroea", "Diarrhea"]
 
-// if(condition.misspelling) {
-//     <em>${condition.misspelling}</em>
-// }
+// as strings
+"synonyms": "Runs, shits, squits, runny poo, bad poo, loose poo, dodgy poo, bad guts, loose guts, dodgy guts, loose stools, runny stools, dodgy stools, loose bowels, dodgy dowels, dodgy tummy, bad tummy, dodgy stomach, bad stomach",
+"misspelling": "Diarhoea, Diarroea, Diarrhea, Diarrhear, Diarhea, Diarhear, Diarhrear, Diarhearh, Diahrearh, Diarrea, Diarea, Diarear, Diarrear, Direar, Direarr, Direa, Direah, Direarh, dyo, dya, dier, diya"
 
-
-
-// const topSearches = document.querySelector('.top-searches');
-// const navAtoZ = document.querySelector('.nhsuk-nav-a-z');
-// const filterAtoZ = document.querySelector('.nhsuk-a-z-filter');
-// const showFiltersButton = document.querySelector('.show-hidden-filters');
-
-// // Search the terms
-// let allConditions = document.querySelector('.nhsuk-list--full');
-// let allConditionsPanel = document.querySelector('.nhsuk-list-panel__list');
-// let allConditionLinks = Array.from(allConditions.querySelectorAll('.nhsuk-list-panel__link'));
-
-// let results = document.querySelector('.nhsuk-list-panel__list--results');
-
-// const reusltsWrapper = document.querySelector('.nhsuk-list-panel__list--wrapper');
-// reusltsWrapper.style.display = "none";
-
-// function findMatches(searchTerm, healthAz) {
-
-//     const allOtherConditions = [];
-//     return healthAz.filter(condition => {
-//         const regex = new RegExp(searchTerm, 'gi');
-//         return condition.innerHTML.match(regex);
-//     })
-
-// }
-
-// function showMatches() {
-
-//     const matchArray = findMatches(this.value, allConditionLinks);
-//     const checkLength = searchConditions.value;
-//     matchArray.length;
-
-//     const html = matchArray.map(condition => {
-//         const regex = new RegExp(this.value, 'gi');
-//         const conditionName = condition.innerHTML.replace(regex, `<span class="search_highlight">${this.value}</span>`);
-//         return `
-//             <li class='nhsuk-list-panel__item'>
-//                 <a class="nhsuk-list-panel__link">
-//                     ${conditionName}
-//                 </a>
-//             </li>
-//         `;
-//     }).join('');
-    
-//     if (checkLength.length >= 1) {
-//         results.innerHTML = html;
-//         reusltsWrapper.style.display = "block";
-//         filteredTotal.innerHTML = `
-//             <strong><span style="color: #000;">${matchArray.length}</span></strong> matches
-//         `;     
-//         results.style.display = "block";
-//     } else {
-//         reusltsWrapper.style.display = "none";
-//         filteredTotal.innerHTML = " ";
-//     }
-    
-//     if ((checkLength.length >= 1) && (matchArray.length === 0)) {
-//         results.innerHTML = `
-//         <li class='nhsuk-list-panel__item'>
-//             <a class="nhsuk-list-panel__link">
-//                 No matches, please try filtering or check the spelling.
-//             </a>
-//         </li>
-//     `;
-//     }
-
-
-// }
-
-// searchConditions.addEventListener('change', showMatches);
-// searchConditions.addEventListener('keyup', showMatches);
-
-
-
-
-
-// // Temp code to make a proof of concept 
-// const sexualHealth = document.querySelector('.sexual-health');
-// let isActive = false;
-// let sexualHealthConditions = ['Balanitis', 'Chlamydia', 'Gonorrhoea', 'HIV', 'Sexually transmitted infections (STIs)', 'Thrush', 'Vaginal discharge'];
-
-// sexualHealth.addEventListener('click', function(e) {    
-//     this.classList.toggle('sexual-health--active');
-
-//     const sexualHealthList = document.querySelector('.sexual-health-list');
-//     sexualHealthList.classList.toggle('sexual-health-list--active');
-//     sexualHealthList.querySelector('.nhsuk-list-panel__list--results');
-   
-//     const fullList = document.querySelector('.full-list');
-//     fullList.classList.toggle('full-list--hidden');
-   
-//     results.style.display = "block";
-
-// })
+*/
